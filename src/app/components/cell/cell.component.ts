3b49769a933c724bbbbba8dcca2bce04eac7c0f0
Aziz <aziz.abdurahimov151002@gmail.com>
2022-07-25 14:47:56 +0500

    refactor: remove empty constructors

--- src/app/components/cell/cell.component.ts
+++ src/app/components/cell/cell.component.ts
@@ -7,11 +7,8 @@ import { Cell } from 'src/app/shared/models/game/Cell';
   templateUrl: './cell.component.html',
   styleUrls: ['./cell.component.scss'],
 })
-export class CellComponent{
+export class CellComponent {
   @Input() cell!: Cell;
   @Output() dragStarted: EventEmitter<void> = new EventEmitter();
   @Output() dragEnded: EventEmitter<CdkDragEnd> = new EventEmitter();
-
-  constructor() {}
-
 }
