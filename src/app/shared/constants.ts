8f75ba395af56fece36bc2e6f7735b3dabf04f92
Aziz <aziz.abdurahimov151002@gmail.com>
2022-08-11 14:33:01 +0500

    fix: change ws connection to wss

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://chess-server-002.herokuapp.com/';
-export const WS_ENDPOINT = 'ws://chess-server-002.herokuapp.com/';
+export const WS_ENDPOINT = 'wss://chess-server-002.herokuapp.com/';
 export const CELL_WIDTH = 64;
 export const enum CastlingDirection {
   LEFT,
